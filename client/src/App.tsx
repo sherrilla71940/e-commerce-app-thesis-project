e0f2cfca79f0605932b595de3614eaea9661e4d7
lvbn <luiz.neto.v@gmail.com>
2023-05-08 08:54:47 -0300

    [FE][landing page][creating routes]

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
+import { Outlet } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Navbar />
+      <div id="detail">
+        <Outlet />
+      </div>
+
     </>
   );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,8 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
+import StoreItems from "./components/store-items/StoreItems";
+
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/products",
+        element: <StoreItems />,
+      },
+      {
+        path: "/:id",
+        element: <h1>TESTING PAGE</h1>,
+      },
+    ]
+  }
+]);
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
