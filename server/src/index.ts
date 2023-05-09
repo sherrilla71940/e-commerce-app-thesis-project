eb6001aeeac268fc058763c345e6182836e7b895
Aaron <sherrilla71940@gmail.com>
2023-05-09 20:51:24 +0800

    [backend][index.ts] establish connection to sequelize database named thesis which I initially created in psql, along with the server connection

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,22 @@
 import { app, port } from "./app";
+import { Sequelize } from "sequelize";
 // IIFE Immediate Invoked Function Expression
 (async function bootstrap() {
   // await db.sequelize.sync();
-  app.listen(3000, () => {
-    console.log(`Express is listening at http://localhost:${port} 🚀`);
+  app.listen(3000, async () => {
+    try {
+      const sequelize = new Sequelize("thesis", "aaronsherrill", "", {
+        host: "localhost",
+        dialect: "postgres",
+      });
+      await sequelize.authenticate();
+      console.log("Successful connection to thesis database in postgres!");
+      console.log(`Express is listening at http://localhost:${port} 🚀`);
+    } catch (e: unknown) {
+      if (e instanceof Error) console.log(e.message);
+      else {
+        console.log("ran into an error in db or server connection");
+      }
+    }
   });
 })();
