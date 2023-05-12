6f585be53f1318134db6d758af500a283c411c63
Xavier Malbran <malbranjavier@gmail.com>
2023-05-12 20:00:59 +0200

    [BE][Model][Transaction basket product model]

--- server/src/models/transactionBasket-model.ts
+++ server/src/models/transactionBasket-model.ts
@@ -4,9 +4,11 @@ import {
   Model,
   ForeignKey,
   BelongsTo,
+  HasMany
 } from "sequelize-typescript";
 import { type TransactionBasketType } from "../../../global-types/index";
 import User from './user-model';
+import TransactionBasketProduct from "./transactionBasketProduct-model";
 
 @Table
 class TransactionBasket extends Model<TransactionBasketType> {
@@ -18,10 +20,13 @@ class TransactionBasket extends Model<TransactionBasketType> {
   buyerId: string;
 
   @Column
-  date: number;
+  date: Date;
 
   @BelongsTo(() => User)
   buyer: User;
+
+  @HasMany(() => TransactionBasketProduct)
+  basketProducts: TransactionBasketProduct[];
 }
 
 export default TransactionBasket;
