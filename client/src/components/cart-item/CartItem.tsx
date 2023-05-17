7fa6ad4367e78fd4d5d7302ab2b9506f7212cb37
Aaron <sherrilla71940@gmail.com>
2023-05-18 00:36:54 +0800

    [fe] make endpoints match correctly

--- client/src/components/cart-item/CartItem.tsx
+++ client/src/components/cart-item/CartItem.tsx
@@ -1,44 +1,47 @@
-import styles from './CartItem.module.css'
-import { useCartSlice } from '../../zustand/ShoppingCartSlice'
-import { userStore } from '../../zustand/UserStore'
-import { deleteProductFromShoppingCart, getShoppingCartProduct } from '../../services/shopping-cart-service'
-import { useEffect, useState } from 'react'
-import { ProductType } from '../../../../global-types/product'
-import { ShoppingCartProductType } from '../../../../global-types/shopping-cart-product'
+import styles from "./CartItem.module.css";
+import { useCartSlice } from "../../zustand/ShoppingCartSlice";
+import { userStore } from "../../zustand/UserStore";
+import {
+  deleteProductFromShoppingCart,
+  getShoppingCartProduct,
+} from "../../services/shopping-cart-service";
+import { useEffect, useState } from "react";
+import { ProductType } from "../../../../global-types/product";
+import { ShoppingCartProductType } from "../../../../global-types/shopping-cart-product";
 
-export default function CartItem ({cartItem}: {cartItem: ShoppingCartProductType}) {
+export default function CartItem({
+  cartItem,
+}: {
+  cartItem: ShoppingCartProductType;
+}) {
+  const id = userStore((state) => state.id);
 
-  const id = userStore((state) => state.id)
+  const increaseQuantity = useCartSlice((state) => state.increaseQuantity);
+  const decreaseQuantity = useCartSlice((state) => state.decreaseQuantity);
+  const removeFromCart = useCartSlice((state) => state.removeFromCart);
 
-  const increaseQuantity = useCartSlice((state) => state.increaseQuantity)
-  const decreaseQuantity = useCartSlice((state) => state.decreaseQuantity)
-  const removeFromCart = useCartSlice((state) => state.removeFromCart)
-
-  console.log('->', cartItem.productId)
-  const [fetchedItem, setFetchedItem] = useState<ProductType>()
+  console.log("->", cartItem.productId);
+  const [fetchedItem, setFetchedItem] = useState<ProductType>();
 
   useEffect(() => {
-
     const fetchShoppingCartProduct = async () => {
-
       try {
-        const shoppingCartProducts = await getShoppingCartProduct({productId: cartItem.productId})
+        const shoppingCartProducts = await getShoppingCartProduct(
+          cartItem.productId
+        );
         // console.log('shoppingCartProducts: ', shoppingCartProducts)
 
-
         if (shoppingCartProducts.name) {
-          setFetchedItem(shoppingCartProducts)
+          setFetchedItem(shoppingCartProducts);
           // console.log('PRODUCT SC: ', shoppingCartProducts)
         }
-
-      } catch(error) {
-        console.log(error)
+      } catch (error) {
+        console.log(error);
       }
-    }
+    };
 
-    fetchShoppingCartProduct()
-
-  }, [])
+    fetchShoppingCartProduct();
+  }, []);
 
   // console.log('CART ITEM: ', cartItem)
 
@@ -46,7 +49,6 @@ export default function CartItem ({cartItem}: {cartItem: ShoppingCartProductType
     <div className={styles.container}>
       <div className={styles.img}></div>
       <div className={styles.itemInfo}>
-
         <div className={styles.left}>
           <p className={styles.name}>{fetchedItem?.name}</p>
           {/* <p className={styles.size}>{fetchedItem?.size}</p> */}
@@ -57,23 +59,31 @@ export default function CartItem ({cartItem}: {cartItem: ShoppingCartProductType
           <p
             className={styles.decrease}
             // onClick={() => decreaseQuantity(cartItem)}
-          >-</p>
+          >
+            -
+          </p>
           <p className={styles.size}>{cartItem?.productQuantity}</p>
           <p
             className={styles.increase}
             // onClick={() => increaseQuantity(cartItem)}
-          >+</p>
+          >
+            +
+          </p>
         </div>
 
         <p
           className={styles.delete}
           onClick={() => {
-            deleteProductFromShoppingCart({userId: id, productId: cartItem.productId})
-            removeFromCart(cartItem.id)
+            deleteProductFromShoppingCart({
+              userId: id,
+              productId: cartItem.productId,
+            });
+            removeFromCart(cartItem.id);
           }}
-        >+</p>
-
+        >
+          +
+        </p>
       </div>
     </div>
-  )
+  );
 }
