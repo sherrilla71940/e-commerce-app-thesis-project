b7def4204ee23c0c6671845f6663e98c92001ac5
Ale Paz <evelia.salgadopaz@gmail.com>
2023-05-18 11:10:42 +0200

    [FE] shoppin cart visible from other pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -61,6 +61,7 @@ const { loggedIn } = userStore()
           <Route path={'/sell'} element={
             <>
               <Navbar />
+              <ShoppingCart />
               <Menu/>
               <ProductForm/>
             </>
@@ -69,6 +70,7 @@ const { loggedIn } = userStore()
           <Route path={`/sellers/${id}`} element={
             <>
               <Navbar />
+              <ShoppingCart />
               <Menu/>
               <SellerStore/>
             </>
